240011e51e68a473eb914b3d38e6361e57c1b555
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-08-30 21:02:49 -0400

    added drinks route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //REQUIRE DEPENDENCIES
 ////////////////////////
 const express = require('express');
-
+const drinks = require("./models/drinks.js")
 
 ////////////////////////
 //INITIALIZING EXPRESS APP
@@ -13,8 +13,15 @@ const app = express();
 ////////////////////////
 //DECLARE ROUTES
 ////////////////////////
+
+//welcome page/landing page
 app.get("/", (req, res) => {
     res.send("Welcome to the GitPub App!")
+})
+
+//drinks json page
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
 })
 ////////////////////////
 //CAN YOU HEARRRRR MEEEE
